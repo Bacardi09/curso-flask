452fc59c84450e66a7bdcbb0eee531f717fb6b9b
Bacardi09 <diegoalejandrobb1@bmail.com>
2025-10-18 21:33:53 -0500

    creación de la session para validad usuarios

---
+++ notes_app/auth/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, render_template, redirect, url_for, flash, session
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        if username == "admin":
+            session["user"] = username
+            return redirect (url_for("notes.index"))
+        else:
+            flash("Usuario no existe", "error")
+    return render_template("login.html")
